ea2c1f3d8f6b27a1587d8c6d6e48c12f21c57708
Wilson <31364471@terciariourquiza.edu.ar>
2024-05-07 19:28:05 -0300

    fortnite page improved

--- clase07/fortnite/script.js
+++ clase07/fortnite/script.js
@@ -11,5 +11,15 @@ fetch('https://fortniteapi.io/v2/items/list?lang=es', {
 
 
 function processData(data){
+    data.items.forEach(dato => {
+        item = document.createElement('div');
+        item.classList.add('m-2', 'col-8', 'cuadro', 'd-flex', 'flex-column', 'align-items-center', 'justify-content-center');
+
+        item.innerHTML = '<p class="noMarginText"><b>Nombre del item: </b></p>' + '<p class="noMarginText">' + dato.name + '</p>' + '<img src="' + dato.images.icon + '" class="imgItem" >' + '<p class="noMarginText"><b>Rareza: </b>' + dato.rarity.name + '</p>';
+        
+        document.querySelector('#board').appendChild(item);
+
+    });
+
     console.log(data);
 }
